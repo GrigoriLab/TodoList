924589b9ae41fa0492e40f69d8beecf1b51db5b3
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-08-04 22:41:56 +0400

    Add icon and check for running.

--- TodoList/main.cpp
+++ TodoList/main.cpp
@@ -1,9 +1,31 @@
 #include "todolist.h"
 #include <QtWidgets/QApplication>
 
+#include <windows.h>
+#include <QMessageBox>
+
+bool isRunning()
+{
+	HANDLE hHandle = CreateMutex(NULL, FALSE, L"TodoListRunningMutex");
+
+	if (ERROR_ALREADY_EXISTS == GetLastError())
+	{
+		QMessageBox messageBox;
+		messageBox.warning(0, "Already running", "TodoList is already running!");
+		return true;
+	}
+	return false;
+}
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	if (isRunning())
+		return 0;
+
+	a.setApplicationName("TodoList");
+	a.setWindowIcon(QIcon(":/TodoList/Resources/icon.png"));
+
 	TodoList w;
 	w.show();
 	return a.exec();
