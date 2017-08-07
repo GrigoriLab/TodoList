c8add927b670b9bac194512e53b6980cac897c95
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-08-07 16:40:08 +0400

    Add tasks in the list.

--- TodoList/todolist.cpp
+++ TodoList/todolist.cpp
@@ -1,12 +1,27 @@
 #include "todolist.h"
 #include "addDialog.h"
 #include <qapplication.h>
+#include <QStringListModel.h>
+#include <QListView>
 
 TodoList::TodoList(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 	connect(ui.pushButtonAdd, &QPushButton::clicked, this, &TodoList::Add);
+	QStringListModel *model = new QStringListModel(this);
+
+	QStringList list;
+
+	list << "First Task"<<"Second Task"<<"";
+
+	model->setStringList(list);
+
+	ui.listViewPlaned->setModel(model);
+	ui.listViewPlaned->setEditTriggers(QAbstractItemView::AllEditTriggers);
+	//ui.listViewPlaned->setFlags(ui.listViewPlaned->flags() | Qt::ItemIsEditable);
+	connect(ui.listViewPlaned, &QListView::editTriggers, this, &TodoList::editTrigger);
+	//ui.listViewPlaned->setEditTriggers(QAbstractItemView::NoEditTriggers);
 }
 
 TodoList::~TodoList()
@@ -18,4 +33,9 @@ void TodoList::Add()
 {
 	AddDialog deltaDialog(this);
 	deltaDialog.exec();
+}
+
+void TodoList::editTrigger()
+{
+	QString str = "Breake Here";
 }
--- TodoList/todolist.h
+++ TodoList/todolist.h
@@ -14,6 +14,7 @@ public:
 private slots:
 
 	void Add();
+	void editTrigger();
 
 private:
 	Ui::TodoListClass ui;
